9da523647c3609c8dd0176dbd3c55d5bc6d1c167
Harsh-0805 <harshbadala0805@gmail.com>
2023-10-09 00:42:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
           <div className="h-full bg-gradient-to-b from-[rgba(0,0,0,1)] via-[rgba(0,0,0,0.7)_55.96%] to-[rgba(0,0,0,0.2)]">
             <div className="w-full h-screen flex justify-center overflow-hidden">
               <div className="w-full max-w-6xl 2xl:max-w-[95%] px-6 py-16">
-                <div className="flex flex-row gap-3 items-center justify-center text-4xl xs:text-5xl lg:text-6xl font-semibold">
-                  <img src={ecellLogo} alt="Ecell" className="w-1/3" />
-                  <img src={jugaad} alt="Jugaad" className="w-1/3" />
+                <div className="flex flex-col gap-3 items-center justify-center text-4xl xs:text-5xl lg:text-6xl font-semibold">
+                  <img src={ecellLogo} alt="Ecell" className="w-1/4" />
+                  <img src={jugaad} alt="Jugaad" className="w-3/4" />
                 </div>
 
                 <span
@@ -22,6 +22,10 @@ function App() {
                 >
                   <span>Coming</span>
                   <span> Soon!!</span>
+                </span>
+                <span className="flex mt-16 flex-col lg:flex-row gap-2 justify-center text-3xl xl:text-9xl text-white text-center w-full col-span-3 uppercase tracking-wider">
+                  <span>Paisa kamaya nhi</span>
+                  <span>Banaya jata hai</span>
                 </span>
               </div>
             </div>
